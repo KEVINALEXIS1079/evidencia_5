81b82182c714653366f696d7188e5d9038acc8f2
KEVINALEXIS1079 <kevinalexiscastaneda4@gmail.com>
2024-06-21 13:16:18 -0500

    estoy acomodando los datos que se pediran con la ayuda del for limitandolo a 7 personas

--- evidencia5.php
+++ evidencia5.php
@@ -1,12 +1,34 @@
 <?php
 echo "INICIO DE SESION.";
 
-$nombre = readline("\ningrese su nombre:");
+$nom = readline("\ningrese su nombre:");
 $contra = readline("ingrese su contraseña: ");
+$empleados = [];
 
-if ($nombre == "kevin" && $contra == 1234) {
-    for ($i=0; $i < 5; $i++) { 
-        echo $i;
+if ($nom == "kevin" && $contra == 1234) {
+    for ($i=0; $i <=7; $i++) { 
+        echo "Persona $i:\n";
+        $nombre = readline("nombre: ");
+        $dni = readline("DNI ");
+        $estatura = readline("estatura");
+        $edad = readline("edad ");
+        $genero = readline("genero M O F");
+        $peso = readline("peso kg");
+        $fuma = readline("fuma si o no");
+
+        $empleado =[
+
+            'nombre' => $nombre,
+            'dni' => $dni,
+            'estatura' => $estatura,
+            'edad' => $edad,
+            'genero' => $genero,
+            'peso' => $peso,
+            'fumador' =>$fuma,
+        ];
+
+
+
     }
 }else {
     echo "el inicio de sesion es incorrecto verifique el nombre o la contraseña";
