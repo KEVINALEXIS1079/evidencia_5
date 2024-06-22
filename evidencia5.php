bf05f2aa1d2a05363d64e7a911fb32e4620da7f7
KEVINALEXIS1079 <kevinalexiscastaneda4@gmail.com>
2024-06-22 13:36:55 -0500

    se termino es sistema de busqueda de empleados ahora quiero agregar un menu con la selesion multiple del match

--- evidencia5.php
+++ evidencia5.php
@@ -6,15 +6,18 @@ $contra = readline("ingrese su contraseña: ");
 $empleados = [];
 
 if ($nom == "kevin" && $contra == 1234) {
-    for ($i=0; $i <=4; $i++) { 
+
+    $n = readline("ingrese el numero de empleados a registrar: ");
+
+    for ($i=0; $i <=$n; $i++) { 
         echo "Persona $i:\n";
         $nombre = readline("nombre: ");
-        $dni = readline("DNI ");
-        $estatura = readline("estatura");
-        $edad = readline("edad ");
-        $genero = readline("genero M O F");
-        $peso = readline("peso kg");
-        $fuma = readline("fuma si o no");
+        $dni = readline("DNI: ");
+        $estatura = readline("estatura: ");
+        $edad = readline("edad: ");
+        $genero = readline("genero M O F: ");
+        $peso = readline("peso kg: ");
+        $fuma = readline("fuma si o no: ");
 
         $empleado =[
 
@@ -26,10 +29,23 @@ if ($nom == "kevin" && $contra == 1234) {
             'peso' => $peso,
             'fumador' =>$fuma,
         ];
-
-
-
+        $empleados[] = $empleado;
     }
+    $nombreBuscado = readline("ingrese el nombre del empleado a buscar. ");
+        $dniBuscado = readline("ingrese el DNI del trabajador. ");
+
+        foreach ($empleados as $empleado) {
+            if ($empleado['nombre'] == $nombreBuscado && $empleado['dni'] == $dniBuscado) {
+                 echo "Nombre: " . $empleado['nombre'] . "\n";
+                 echo "DNI: " . $empleado['dni'] . "\n";
+                 echo "Peso: " . $empleado['peso'] . "\n";
+                 echo "Edad: " . $empleado['edad'] . "\n";
+                 echo "Altura: " . $empleado['estatura'] . "\n";
+                 echo "Género: " . $empleado['genero'] . "\n";
+                 echo "¿Fuma?: " . $empleado['fumador']  . "\n";
+        break;
+    }
+}
 }else {
     echo "el inicio de sesion es incorrecto verifique el nombre o la contraseña";
 }
